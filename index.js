27ec2708f93ae0ba8122ab293c4c980aecc9c66f
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-11 21:09:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-function fetchBooks() {
+// fetch('http://api.open-notify.org/astros.json')
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(json) {
+//   console.log(json)
+// });
 
+function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json[4]);
+      renderBooks(json);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
